5a81f3f0143f2141bab5a3a78c7fc329fd782126
OWOAGDN <jdjb90@hotmail.com>
2024-12-10 16:10:56 +0000

    search bar details

---
+++ src/SearchBar.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function SearchBar() {
+  return (
+    <div className="">
+      <form>
+        <input 
+        type="text"
+        placeholder="Search"
+        key="search"
+        value={}>
+          <img src=""></img>
+        </input>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
