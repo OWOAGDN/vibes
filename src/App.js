1437e0be6fb45174913829f650215a6f2460d63f
OWOAGDN <jdjb90@hotmail.com>
2024-12-12 15:28:49 +0000

    Core components files created and folders setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchBar from './SearchBar/SearchBar';
+import Playlist from './Playlist/Playlist';
+import SearchResults from './SearchResults/SearchResults';
+import Tracklist from './Tracklist/Tracklist';
 
 function App() {
   return (
@@ -10,6 +12,13 @@ function App() {
       </header>
       <main>
         <SearchBar />
+        <div className="PlayArea">
+          <SearchResults />
+          <div className="ListArea">
+          <Playlist />
+          <Tracklist />
+          </div>
+        </div>
       </main>
     </div>
   );
