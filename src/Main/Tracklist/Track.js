6e0d9189a0520152a204f5679dfa06b73ce62d7a
Jordan Dwaine <jdjb90@hotmail.com>
2025-03-03 16:02:19 +0000

    Song styling added

--- src/Main/Tracklist/Track.js
+++ src/Main/Tracklist/Track.js
@@ -10,13 +10,13 @@ function Track( {track, addPlaylistSongs, type, deletePlaylistSong}) {
   }
     return (
     <li className={styles.song}>
+        <div className={styles.imageContainer} ><img src={track.image} alt={track.name}></img></div>
         <div className={styles.songContainer}>
-          <p>Song: {track.name}</p>
-          <p>Artist: {track.artist}</p>
-          <p>Album: {track.album}</p>
-          <div className={styles.imageContainer} ><img src={track.image} alt={track.name}></img></div>
+          <p className={styles.trackName}>{track.name}</p>
+          <p className={styles.artistName}>{track.artist}</p>
+          <p className={styles.albumName}>{track.album}</p>
         </div>
-        {type === 'result' ? <button onClick={addSong}>+</button> : <button onClick={deleteSong}>-</button> } 
+        <div className={styles.buttonContainer}>{type === 'result' ? <button onClick={addSong}>+</button> : <button onClick={deleteSong}>-</button> }</div>
     </li>
     );
 }
